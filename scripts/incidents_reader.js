829fca2ad421d0689f5db704d3e9f787dec81630
Rubio Vaughan <rubio.vaughan@geodan.nl>
2017-11-08 17:04:40 +0100

    updated incident reader

--- scripts/incidents_reader.js
+++ scripts/incidents_reader.js
@@ -1,74 +1,119 @@
-var fs        = require('fs')
-  , zlib          = require('zlib')
-  , path      = require('path')
-  , XmlStream = require('/usr/local/lib/node_modules/xml-stream')
-  , pg            = require('/usr/local/lib/node_modules/pg')
-  , copy      = require('/usr/local/lib/node_modules/pg-copy-streams')
-  , request   = require('/usr/local/lib/node_modules/request');
+var fs		= require('fs')
+  , zlib		  = require('zlib')
+  , path	  = require('path')
+  , XmlStream = require('xml-stream')
+  , { Pool }  = require('/usr/local/lib/node_modules/pg')
+  , copy	  = require('/usr/local/lib/node_modules/pg-copy-streams')
+  , request   = require('/usr/local/lib/node_modules/request')
+  , SQL	   = require('sql-template-strings');
 
+var incidentClassTypes = {
+	'AbnormalTraffic': 'abnormalTrafficType',
+	'Accident': 'accidentType',
+	'AuthorityOperation': 'authorityOperationType',
+	'DisturbanceActivity': 'disturbanceActivityType',
+	'PublicEvent': 'publicEventType',
+	'PoorEnvironmentConditions': 'poorEnvironmentType',
+	'NonWeatherRelatedRoadConditions': 'nonWeatherRelatedRoadConditionType',
+	'WeatherRelatedRoadConditions': 'weatherRelatedRoadConditionType',
+	'EquipmentOrSystemFault': 'faultyEquipmentOrSystemType',
+	'AnimalPresenceObstruction': 'animalPresenceType',
+	'EnvironmentalObstruction': 'environmentalObstructionType',
+	'GeneralObstruction': 'obstructionType',
+	'InfrastructureDamageObstruction': 'infrastructureDamageType',
+	'VehicleObstruction': 'vehicleObstructionType'
+}
 
+function readXML() {
+	return new Promise(function(resolve, reject) {
+		var nodes = [];
+		var stream = request.get('http://opendata.ndw.nu/incidents.xml.gz')
+				.pipe(zlib.createGunzip());
+		var xml = new XmlStream(stream, 'utf8');
+		xml.collect('values');
+		xml.on('updateElement: situation', function(node) {
+			nodes.push(node);
+		});
+		xml.on('error', async function(message) {
+			reject('XML parsing failed: ' + message);
+		});
+		xml.on('end', async function(){
+			console.log('XML closed');
+			resolve(nodes);
+		});
+	});
+}
 
-
-var _db;
 //Prepare PG
-pg.on('error', function (err) {
-  console.log('Database error!', err);
+const pool = new Pool({
+	host: 'localhost',
+	port: 5433,
+	user: 'postgres',
+	password: '',
+	database: 'research'
 });
-var connstring = "tcp://postgres@localhost/research";
 
+(async () => {
+	// note: we don't try/catch this because if connecting throws an exception
+	// we don't need to dispose of the client (it will be undefined)
+	const client = await pool.connect();
 
+	try {
+		var nodes = await readXML();
+		console.log('read ' + nodes.length + ' nodes');
+		await client.query('BEGIN');
+		await client.query('UPDATE ndw.incidents SET active = 0 WHERE active = 1');
 
-pg.connect(connstring, function(err, client) {
-                if (err){
-              console.log('Connecting error: ',err);
-        }
-        var querystring = copy.from('COPY ndw.incidents FROM STDIN');
-        var pgstream = client.query(querystring);
-        pgstream.on('error',function(e){
-                        console.log('Stream error: ',e)
-        });
-        pgstream.on('end', function() {
-                        console.log('Stream closed');
-        });
+		var counter = 0;
+		
+		for(var i=0; i<nodes.length; i++) {
+			const node = nodes[i];
+			//console.log(node);
+			var observationtime = node.situationRecord.situationRecordObservationTime;
+			var probabilityofoccurrence = node.situationRecord.probabilityOfOccurrence;
+			var id = node.$.id;
+			var version = node.$.version;
+			var source = node.situationRecord.source.sourceName.values[0].value.$text;
+			var location = node.situationRecord.groupOfLocations.locationForDisplay;
+			var incidentclass = node.situationRecord.$['xsi:type'];
+			var incidenttype = 'Unknown';
+			if (incidentClassTypes.hasOwnProperty(incidentclass)) {
+				var typeField = incidentClassTypes[incidentclass];
+				incidenttype = node.situationRecord[typeField];
+			}
+			
+			//console.log(observationtime, id, version, probabilityofoccurrence, source, accidenttype,location);
 
-        var counter = 0;
-                //var stream = fs.createReadStream('trafficspeed.xml');
-                var stream = request.get('http://opendata.ndw.nu/incidents.xml.gz')
-                        .pipe(zlib.createGunzip());
+			//console.log(counter++,'---------------------------------');
 
-                var xml = new XmlStream(stream, 'utf8');
-                xml.collect('values');
-                xml.on('updateElement: situation', function(node) {
-                        //console.log(node);
-                        var observationtime = node.situationRecord.situationRecordObservationTime;
-                        var probabilityofoccurrence = node.situationRecord.probabilityOfOccurrence;
-                        var id = node.$.id;
-                        var version = node.$.version;
-                        var source = node.situationRecord.source.sourceName.values[0].value.$text;
-                        var location = node.situationRecord.groupOfLocations.locationForDisplay;
-                        var accidenttype = node.situationRecord.accidentType;
-                        //console.log(observationtime, id, version, probabilityofoccurrence, source, accidenttype,location);
+			//Stream to db
+			var querystring = SQL`
+				INSERT INTO ndw.incidents
+				(id, version, active, observationtime, probabilityofoccurrence, source, 
+					incidentclass, incidenttype, latitude, longitude, geom)
+				VALUES (
+					${id},
+					${version},
+					1,
+					${observationtime},
+					${probabilityofoccurrence},
+					${source},
+					${incidentclass},
+					${incidenttype},
+					${location.latitude},
+					${location.longitude},
+					ST_SetSRID(ST_MakePoint(${location.longitude}, ${location.latitude}), 4326)
+				)
+				ON CONFLICT ON CONSTRAINT incidents_pkey DO UPDATE SET active = 1;
+			`;
+			await client.query(querystring);
+		};
+		await client.query('COMMIT');
+	} catch (e) {
+		await client.query('ROLLBACK');
+		throw e;
+	} finally {
+		client.release();
+	}
+})().catch(e => console.error(e.stack))
 
-                        //console.log(counter++,'---------------------------------');
-
-                        //Stream to db
-                        var writestring = id + '\t' +
-                                version + '\t' +
-                                observationtime + '\t' +
-                                probabilityofoccurrence + '\t' +
-                                source + '\t' +
-                                accidenttype + '\t' +
-                                location.latitude + '\t' +
-                                location.longitude + '\n';
-                        //console.log('Copying:', writestring);
-                        pgstream.write(writestring);
-                });
-                xml.on('error', function(message) {
-                        console.log('Parsing failed: ' + message);
-                });
-                xml.on('end',function(){
-                        console.log('XML closed');
-                        pgstream.end();
-                        client.end();
-                });
-});
